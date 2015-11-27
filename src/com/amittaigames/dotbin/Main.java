01e6e3c9c97dd0e5896e90801e953485b8edd29b
Gandalf1209 (Jonah) <criminal.jba@gmail.com>
2015-11-26 20:37:31 -0600

    Commit #0002
    Fixed bug due to Ludum GL. That will be fixed soon also.

--- src/com/amittaigames/dotbin/Main.java
+++ src/com/amittaigames/dotbin/Main.java
@@ -5,8 +5,9 @@ import com.amittaigames.ludumgl.Window;
 import com.amittaigames.ludumgl.graphics.Render;
 
 public class Main extends CoreGame {
-
+	
 	public static void main(String[] args) {
+		Window.config(1, 1);
 		Window.init(".BIN", 800, 600, new Main(), 60);
 	}
 	
