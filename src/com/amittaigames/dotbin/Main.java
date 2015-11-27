8a395f1bb276ea9d071da59512510b56e0c1836a
Gandalf1209 (Jonah) <criminal.jba@gmail.com>
2015-11-26 19:09:05 -0600

    Commit #0001
    First commit, with a basic window, and background. 60 FPS FTW

---
+++ src/com/amittaigames/dotbin/Main.java
@@ -0,0 +1,28 @@
+package com.amittaigames.dotbin;
+
+import com.amittaigames.ludumgl.CoreGame;
+import com.amittaigames.ludumgl.Window;
+import com.amittaigames.ludumgl.graphics.Render;
+
+public class Main extends CoreGame {
+
+	public static void main(String[] args) {
+		Window.init(".BIN", 800, 600, new Main(), 60);
+	}
+	
+	@Override
+	public void init() {
+		
+	}
+
+	@Override
+	public void render(Render r) {
+		r.clear(225, 225, 225);
+	}
+
+	@Override
+	public void update(int delta) {
+		
+	}
+	
+}
